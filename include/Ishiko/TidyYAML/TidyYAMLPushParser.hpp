751298ebfc85a490c2404cc73469cee43f94c480
Xavier Leclercq <xavier@leclercq.dev>
2026-07-23 22:33:28 +0100

    Updates

--- include/Ishiko/TidyYAML/TidyYAMLPushParser.hpp
+++ include/Ishiko/TidyYAML/TidyYAMLPushParser.hpp
@@ -4,4 +4,8 @@
 #ifndef GUARD_ISHIKO_CPP_TIDYYAML_TIDYYAMLPUSHPARSER_HPP
 #define GUARD_ISHIKO_CPP_TIDYYAML_TIDYYAMLPUSHPARSER_HPP
 
+// TODO: I need to implement the parser. For now we just include this and clients will actually use yaml-cpp. So for
+// this library is just a way to put a placeholder for the dependency and a reminder I need to implement this.
+#include <yaml-cpp/yaml.h>
+
 #endif
